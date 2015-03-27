3431d7b3f52166fd2147806e61bb38945efe8489
Charles Talbot <charlietalbot@gmail.com>
2015-03-27 09:53:15 -0400

    building out home controller and routes

---
+++ app/scripts/controllers/HomeController.js
@@ -0,0 +1,7 @@
+'use strict';
+angular.module('AuthApp').controller('HomeController', homeController);
+homeController.$inject = [];
+
+function homeController(){
+  //do something on the home view
+}
